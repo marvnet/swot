f45d95ab15a411240a1919ae10d31fd8d0c4b4b7
MagicMarvMan <marvin.kai@gmx.de>
2019-11-17 21:54:34 +0100

    Remove old script

--- delete_old.js
+++
@@ -1,7 +0,0 @@
-const old_files = [/* OLD FILES HERE */]
-
-const { execSync } = require('child_process');
-
-old_files.forEach((file) => {
-    execSync(`grep -v "${file}" swot.gemspec > swot.gemspec2; mv swot.gemspec2 swot.gemspec`)
-})
