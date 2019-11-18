db0a5747882bc539504e1abdd01519d7af59d525
MagicMarvMan <marvin.kai@gmx.de>
2019-11-18 19:32:59 +0100

    Prettify code

--- src/index.js
+++ src/index.js
@@ -1,79 +1,123 @@
-const extractDomain = require('extract-domain')
-const parseDomain   = require("parse-domain")
+const extractDomain = require("extract-domain")
+const parseDomain = require("parse-domain")
 const fs = require("fs")
 const path = require("path")
 
 let DEBUG = false
 
-if(process.env.NODE_ENV) {
-    if(process.env.NODE_ENV == "development" || process.env.NODE_ENV == "test") DEBUG = true
+if (process.env.NODE_ENV) {
+    if (process.env.NODE_ENV == "development" || process.env.NODE_ENV == "test")
+        DEBUG = true
 }
 
-module.exports.isAcademic = function (subject) {
+module.exports.isAcademic = function(subject) {
     let PARSED_DOMAIN = parseDomain(subject)
-    if(DEBUG) console.log(`Extracted domain: ${PARSED_DOMAIN.domain + "." + PARSED_DOMAIN.tld}`)
-    if(PARSED_DOMAIN) {
+    if (DEBUG)
+        console.log(
+            `Extracted domain: ${PARSED_DOMAIN.domain +
+                "." +
+                PARSED_DOMAIN.tld}`
+        )
+    if (PARSED_DOMAIN) {
         try {
-            if(PARSED_DOMAIN.tld.split(".").length == 2) {
+            if (PARSED_DOMAIN.tld.split(".").length == 2) {
                 PARSED_DOMAIN.tld = PARSED_DOMAIN.tld.split(".")
                 let tld_1 = PARSED_DOMAIN.tld[1]
                 let tld_2 = PARSED_DOMAIN.tld[0]
                 PARSED_DOMAIN.tld = tld_1 + "." + tld_2
             }
-        } catch(e) {
+        } catch (e) {
             // blackhole
         }
-        if(DEBUG) console.log(`Looking up ${path.join(__dirname, "/domains/", PARSED_DOMAIN.tld.replace(".", "/"))}...`)
-        if(fs.existsSync(path.join(__dirname + "/domains/" + PARSED_DOMAIN.tld.replace(".", "/")))) {
-            if(DEBUG) console.log(`Looking up ${path.join(__dirname, "/domains/", PARSED_DOMAIN.tld.replace(".", "/"), "/", PARSED_DOMAIN.domain + ".json")}...`)
-            if(fs.existsSync(path.join(__dirname, "/domains/", PARSED_DOMAIN.tld.replace(".", "/"), "/", PARSED_DOMAIN.domain +  ".json"))) {
+        if (DEBUG)
+            console.log(
+                `Looking up ${path.join(
+                    __dirname,
+                    "/domains/",
+                    PARSED_DOMAIN.tld.replace(".", "/")
+                )}...`
+            )
+        if (
+            fs.existsSync(
+                path.join(
+                    __dirname +
+                        "/domains/" +
+                        PARSED_DOMAIN.tld.replace(".", "/")
+                )
+            )
+        ) {
+            if (DEBUG)
+                console.log(
+                    `Looking up ${path.join(
+                        __dirname,
+                        "/domains/",
+                        PARSED_DOMAIN.tld.replace(".", "/"),
+                        "/",
+                        PARSED_DOMAIN.domain + ".json"
+                    )}...`
+                )
+            if (
+                fs.existsSync(
+                    path.join(
+                        __dirname,
+                        "/domains/",
+                        PARSED_DOMAIN.tld.replace(".", "/"),
+                        "/",
+                        PARSED_DOMAIN.domain + ".json"
+                    )
+                )
+            ) {
                 return true
             } else {
-                if(DEBUG) console.log("School does not exist")
+                if (DEBUG) console.log("School does not exist")
                 return false
             }
         } else {
-            if(DEBUG) console.log("TLD does not exist")
+            if (DEBUG) console.log("TLD does not exist")
             return false
         }
     } else {
-        if(DEBUG) console.log("Domain could not be parsed")
+        if (DEBUG) console.log("Domain could not be parsed")
         return false
     }
 }
 
 module.exports.school = function(subject) {
-    if(this.isAcademic(subject)) {
+    if (this.isAcademic(subject)) {
         let PARSED_DOMAIN = parseDomain(subject)
-        if(PARSED_DOMAIN) {
+        if (PARSED_DOMAIN) {
             try {
-                if(PARSED_DOMAIN.tld.split(".").length == 2) {
+                if (PARSED_DOMAIN.tld.split(".").length == 2) {
                     PARSED_DOMAIN.tld = PARSED_DOMAIN.tld.split(".")
                     let tld_1 = PARSED_DOMAIN.tld[1]
                     let tld_2 = PARSED_DOMAIN.tld[0]
                     PARSED_DOMAIN.tld = tld_1 + "." + tld_2
                 }
-            } catch(e) {
+            } catch (e) {
                 // blackhole
             }
-            return require(`${__dirname}/domains/${PARSED_DOMAIN.tld.replace(".", "/")}/${PARSED_DOMAIN.domain}.json`)
+            return require(`${__dirname}/domains/${PARSED_DOMAIN.tld.replace(
+                ".",
+                "/"
+            )}/${PARSED_DOMAIN.domain}.json`)
         }
     } else return false
 }
 
 // DEPRECATED
-module.exports.school_name = function(subject) { // DEPRECATED
-    if(this.isAcademic(subject)) {
+module.exports.school_name = function(subject) {
+    // DEPRECATED
+    if (this.isAcademic(subject)) {
         let PARSED_DOMAIN = parseDomain(subject)
-        if(PARSED_DOMAIN) {
+        if (PARSED_DOMAIN) {
             try {
-                if(PARSED_DOMAIN.tld.split(".").length == 2) {
+                if (PARSED_DOMAIN.tld.split(".").length == 2) {
                     PARSED_DOMAIN.tld = PARSED_DOMAIN.tld.split(".")
                     let tld_1 = PARSED_DOMAIN.tld[1]
                     let tld_2 = PARSED_DOMAIN.tld[0]
                     PARSED_DOMAIN.tld = tld_1 + "." + tld_2
                 }
-            } catch(e) {
+            } catch (e) {
                 // blackhole
             }
             return this.school(subject).name
